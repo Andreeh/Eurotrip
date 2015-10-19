f37a0838c5f5bc51b83027fc32b0119f8a17ee88
Andree <Hoog16@gmail.com>
2015-10-19 17:39:12 +0200

    added googlemaps

--- app/src/main/java/com/example/karlmosenbacher/eurotrip/EuroTripMainActivity.java
+++ app/src/main/java/com/example/karlmosenbacher/eurotrip/EuroTripMainActivity.java
@@ -1,14 +1,29 @@
 package com.example.karlmosenbacher.eurotrip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class EuroTripMainActivity extends AppCompatActivity {
+    EuroTripMainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_euro_trip_main);
+        mainActivity = this;
+
+        ImageButton start = (ImageButton)findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainActivity, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
