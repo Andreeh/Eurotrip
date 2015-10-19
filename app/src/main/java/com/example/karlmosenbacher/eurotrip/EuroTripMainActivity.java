634f678400205128c5441f2254eed311d88c813a
Karl Mösenbacher <karlmosenbacher@Karls-MacBook-Pro.local>
2015-10-19 16:23:17 +0200

    started project

---
+++ app/src/main/java/com/example/karlmosenbacher/eurotrip/EuroTripMainActivity.java
@@ -0,0 +1,23 @@
+package com.example.karlmosenbacher.eurotrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EuroTripMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_euro_trip_main);
+    }
+
+
+
+
+
+
+
+
+
+
+}
